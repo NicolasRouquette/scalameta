58426c0a1fb296570f44e0f40ef877609fa87061
Eugene Burmako <xeno.by@gmail.com>
2015-01-12 11:53:32 +0100

    accounts for recent changes in scalameta/scalameta

--- plugin/src/main/scala/org/scalameta/example/Example.scala
+++ plugin/src/main/scala/org/scalameta/example/Example.scala
@@ -2,7 +2,6 @@ package org.scalameta.example
 
 import scala.meta.internal.ast._
 import scala.meta.semantic._
-import scala.meta.semantic.errors.throwExceptions
 
 trait Example {
   val global: scala.tools.nsc.Global
